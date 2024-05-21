76ff43714ef408e7286949f7359cabc77fd36c97
hd <13897436954@163.com>
2024-05-21 08:48:36 +0800

    注册功能开发

--- src/main/java/com/nowcoder/community/Mapper/UserMapper.java
+++ src/main/java/com/nowcoder/community/Mapper/UserMapper.java
@@ -6,6 +6,13 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-    @Select("select * from community.user where id = #{id}")
     User selectById(int id);
+
+    User selectByName(String username);
+
+    User selectByEmail(String email);
+
+    int insertUser(User user);
+
+    int updateStatus(int id, int status);
 }
--- src/main/java/com/nowcoder/community/Service/UserService.java
+++ src/main/java/com/nowcoder/community/Service/UserService.java
@@ -2,16 +2,112 @@ package com.nowcoder.community.Service;
 
 import com.nowcoder.community.Mapper.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant{
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
 
     public User findUserById(int id){
         return userMapper.selectById(id);
     }
+
+    public Map<String, Object> register(User user){
+        Map<String, Object> map = new HashMap<>();
+
+        //空值判断
+        if(user == null){
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+        if(StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMsg", "账号不能为空！");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getPassword())){
+            map.put("passwordMsg", "密码不能为空！");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getEmail())){
+            map.put("emailMsg", "邮箱不能为空！");
+            return map;
+        }
+
+        //验证账号
+        User u = userMapper.selectByName(user.getUsername());
+        if(u != null){
+            map.put("usernameMsg", "该账号已存在！");
+            return map;
+        }
+
+        //验证邮箱
+        u = userMapper.selectByEmail(user.getEmail());
+        if(u != null){
+            map.put("emailMsg", "该邮箱已被注册！");
+            return map;
+        }
+
+        //注册用户
+        //将用户信息写入数据库里
+        //1、对密码进行md5的加密
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
+        //2、初始化以及给激活码
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("https://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        userMapper.insertUser(user);
+
+        // 激活邮件
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        // http://localhost:8080/community/activation/101/code
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        String content = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
+
+        return map;
+    }
+
+    //得到是否激活成功的状态码
+    public int activation(int userId, String code){
+        User user = userMapper.selectById(userId);
+        if(user.getStatus() == 1){
+            //已经激活了，再调用这个方法就代表室重复激活了
+            return ACTIVATION_REPEAT;
+        }else if(user.getActivationCode().equals(code)){
+            userMapper.updateStatus(userId, 1);
+            return ACTIVATION_SUCCESS;
+        }else{
+            return ACTIVATION_FAILURE;
+        }
+    }
+
 
 }
