9d91f8fbdcc07cae4d4702e2e0a8386513a0f1c6
hd <13897436954@163.com>
2024-06-25 07:43:36 +0800

    SpringAOP统一记录日志

---
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,44 @@
+package com.nowcoder.community.aspect;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    //连接点，指代的是程序织入的部位
+    @Pointcut("execution(* com.nowcoder.community.Service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+
+    //在业务的一开始，记录日志，格式为：用户（ip地址）在某时刻访问了某功能、某方法
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint){
+        //即记录用户[1,2,3,4]，在[xxx]，访问了[com.nowcoder.community.service.xxx()]
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s]，在[%s]，访问了[%s]", ip, now, target));
+    }
+
+
+}
