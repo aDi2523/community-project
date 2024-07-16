0a53adbc1848330ccd9525bf4c8395c29daf81e3
hd <13897436954@163.com>
2024-07-16 21:43:42 +0800

    使用本地缓存工具Caffeine来缓存热门帖子，优化网站性能

--- src/main/java/com/nowcoder/community/Service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/Service/DiscussPostService.java
@@ -1,14 +1,23 @@
 package com.nowcoder.community.Service;
 
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.nowcoder.community.Mapper.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.util.SensitiveFilter;
+import jakarta.annotation.PostConstruct;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DiscussPostService {
@@ -18,11 +27,77 @@ public class DiscussPostService {
     @Autowired
     private SensitiveFilter sensitiveFilter;
 
+    @Value("${caffeine.posts.max-size}")
+    private int maxSize;
+
+    @Value("${caffeine.posts.expire-seconds}")
+    private int expireSeconds;
+
+    private static final Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
+
+    //声明帖子列表缓存
+    private LoadingCache<String, List<DiscussPost>> postListCache;
+
+    //声明帖子总数缓存
+    private LoadingCache<Integer, Integer> postRowsCache;
+
+    //定义方法对帖子列表和总数进行初始化
+    @PostConstruct
+    public void init() {
+        //初始化帖子列表缓存
+        postListCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    @Override
+                    public @Nullable List<DiscussPost> load(String key) throws Exception {
+                        if (key == null || key.length() == 0) {
+                            throw new IllegalArgumentException("参数错误！");
+                        }
+
+                        String[] keyStrArr = key.split(":");
+                        if (keyStrArr == null || keyStrArr.length != 2) {
+                            throw new IllegalArgumentException("参数错误！");
+                        }
+
+                        int offset = Integer.parseInt(keyStrArr[0]);
+                        int limit = Integer.parseInt(keyStrArr[1]);
+
+                        logger.info("load post list from DB.");
+                        return discussPostMapper.selectDiscussPosts(0, offset, limit, 1);
+                    }
+                });
+
+        //出事阿虎帖子总数缓存
+        postRowsCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Override
+                    public @Nullable Integer load(Integer key) throws Exception {
+                        logger.info("load post rows from DB.");
+                        return discussPostMapper.selectDiscussPostRows(key);
+                    }
+                });
+    }
+
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit, int orderMode) {
+        if (userId == 0 && orderMode == 1) {
+            //仅在首页查询以及为最热帖子的时候在本地缓存中获取帖子列表
+            return postListCache.get(offset + ":" + limit);
+        }
+
+        logger.info("load post list from DB.");
         return discussPostMapper.selectDiscussPosts(userId, offset, limit, orderMode);
     }
 
     public Integer findDiscussPostRows(int userId) {
+        if(userId == 0){
+            //仅在首页查询的时候在本地缓存中获取帖子的总行数
+            return postRowsCache.get(userId);
+        }
+
+        logger.info("load post rows from DB.");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
---
+++ src/test/java/com/nowcoder/community/CaffeineTests.java
@@ -0,0 +1,38 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.Service.DiscussPostService;
+import com.nowcoder.community.entity.DiscussPost;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+@SpringBootTest
+class CaffeineTests {
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Test
+    public void initDataForTest() {
+        for (int i = 0; i < 300000; i++) {
+            DiscussPost post = new DiscussPost();
+            post.setUserId(111);
+            post.setTitle("111");
+            post.setContent("aaa");
+            post.setCreateTime(new Date());
+            post.setScore(Math.random() * 2000);
+            discussPostService.addDiscussPost(post);
+        }
+    }
+
+    @Test
+    public void testCache() {
+        System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 0));
+    }
+
+
+}
